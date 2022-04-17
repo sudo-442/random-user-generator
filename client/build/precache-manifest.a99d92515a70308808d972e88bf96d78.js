228e38d5e80fc3a856409c524b8dcaa2ef9b81e5
sudo442 <>
2022-04-17 16:43:24 +0000

    Create an optimized production build

---
+++ client/build/precache-manifest.a99d92515a70308808d972e88bf96d78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fabba4eb0bf08607e56f48f60cf1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76216706514b9a653d99",
+    "url": "/static/js/2.b8a49f71.chunk.js"
+  },
+  {
+    "revision": "a632f393b35ad97deebd",
+    "url": "/static/js/main.29654dbc.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
