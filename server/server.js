cf32d8c8eeae81a88aeb7f4061bf54b6f55bfedf
sudo-442 <103894107+sudo-442@users.noreply.github.com>
2022-04-17 12:29:09 -0400

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const config = require("./config");
+const { log } = require("./handlers/logging");
+const { limiter } = require("./middlewares/limiter")
+
+const app = express();
+const port = config.port || 443
+
+app.use(limiter);
+app.use(express.static("client/build"))
+
+app.get("/", (req, res) => {
+	res.sendFile(
+		"index.html",
+		{
+			root: "../client/build"
+		}
+	)
+})
+
+app.listen(port, () => {
+	log(`Server is listening on port ${port}.`)
+})
