2fae43f7d87e691213eea4f07f58e4d7b7bf570a
sudo442 <103894107+sudo-442@users.noreply.github.com>
2022-04-17 16:38:00 +0000

    Change file path to rate limiter

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const config = require("./config");
 const { log } = require("./handlers/logging");
-const { limiter } = require("./middlewares/limiter")
+const { limiter } = require("./middlewares/ratelimit")
 
 const app = express();
 const port = config.port || 443
